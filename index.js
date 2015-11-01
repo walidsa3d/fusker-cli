2a1a1313246ab3a8de42bf28c1fc5063a00de98b
walid <walid.sa3d@gmail.com>
2015-11-01 23:02:31 +0100

    add option to download files

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var Fusker = require('node-fusker').Fusker;
 var program = require('commander');
 var open = require("open");
+var download = require('download-file');
 
 program.version('1.0.0')
     .option('-o, --open', 'open urls in default browser')
+    .option('-d, --download', 'download files')
     .parse(process.argv);
 var fusker = new Fusker();
 var fusks = program.args[0];
@@ -13,8 +15,29 @@ if (program.open) {
     urls.forEach(function(url) {
         open(url);
     });
+} else if (program.download) {
+
+    urls.forEach(function(url){
+download_image(url, ".");
+      });
+   
 } else {
     urls.forEach(function(url) {
         console.log(url);
     });
 }
+
+function download_image(url, dir){
+    var options = {
+        directory: ".",
+    };
+    download(url, options, function(err) {
+        if (err) console.log(err);
+        else console.log('Downloaded ', url);
+    });
+}
+
+
+
+
+
