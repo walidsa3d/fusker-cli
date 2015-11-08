027e131f96ace0695425440c0e0ad978cec3317d
walid <walid.sa3d@gmail.com>
2015-11-08 18:28:56 +0100

    update readme

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
-var Fusker = require('node-fusker').Fusker;
-var program = require('commander');
-var open = require("open");
-var download = require('download-file');
+var Fusker = require('node-fusker').Fusker
+var program = require('commander')
+var open = require('open')
+var download = require('download-file')
+var pkg = require('./package.json')
 
-program.version('1.0.0')
-    .option('-o, --open', 'open urls in default browser')
-    .option('-d, --download', 'download files')
-    .parse(process.argv);
-var fusker = new Fusker();
-var fusks = program.args[0];
-fusker.load(fusks);
-var urls = fusker.getAll();
+program.version(pkg.info)
+  .option('-o, --open', 'open urls in default browser')
+  .option('-d, --download', 'download files')
+  .parse(process.argv)
+var fusker = new Fusker()
+var fusks = program.args[0]
+fusker.load(fusks)
+var urls = fusker.getAll()
 if (program.open) {
-    urls.forEach(function(url) {
-        open(url);
-    });
+  urls.forEach(function (url) {
+    open(url)
+  })
 } else if (program.download) {
-
-    urls.forEach(function(url){
-download_image(url, ".");
-      });
-   
+  urls.forEach(function (url) {
+    download_image(url, '.')
+  })
 } else {
-    urls.forEach(function(url) {
-        console.log(url);
-    });
+  urls.forEach(function (url) {
+    console.log(url)
+  })
 }
 
-function download_image(url, dir){
-    var options = {
-        directory: ".",
-    };
-    download(url, options, function(err) {
-        if (err) console.log(err);
-        else console.log('Downloaded ', url);
-    });
+function download_image (url, dir) {
+  var options = {
+    directory: '.'
+  }
+  download(url, options, function (err) {
+    if (err) console.log(err)
+    else console.log('Downloaded ', url)
+  })
 }
-
-
-
-
-
